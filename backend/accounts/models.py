3e644361f63f47295b9eb001ef9444a929da223c
Parsa <parsa@pop-os.localdomain>
2021-05-26 15:44:14 +0430

    added phone and national ID validators

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -5,6 +5,7 @@ from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
 from accounts.managers import CustomUserManager
+from accounts.validators import mobile_regex, validate_national_ID
 
 
 class CustomUser(AbstractUser):
@@ -33,12 +34,21 @@ class Profile(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
     email_confirmed = models.BooleanField(default=False)
-    mobile = models.CharField(max_length=10, null=True,
-                              blank=True)  # todo: convert to phone-field
+    mobile = models.CharField(max_length=11,
+                              null=True,
+                              blank=True,
+                              validators=[mobile_regex])
     mobile_confirmed = models.BooleanField(default=False)
-    national_id = models.IntegerField(default=None, null=True, blank=True)
+    national_id = models.CharField(max_length=10,
+                                   default=None,
+                                   null=True,
+                                   blank=True,
+                                   validators=[validate_national_ID])
     national_id_confirmed = models.BooleanField(default=False)
-    medical_id = models.IntegerField(default=None, null=True, blank=True)
+    medical_id = models.CharField(max_length=15,
+                                  default=None,
+                                  null=True,
+                                  blank=True)
     medical_id_confirmed = models.BooleanField(default=False)
 
     def __str__(self):
