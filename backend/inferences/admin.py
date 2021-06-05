4e956ecca58533446ff9a7a5817ab9fba0bdd8aa
Dire Titan <titan@pop-os.localdomain>
2021-06-05 16:53:54 +0430

    added COVIDCTInference admin

--- backend/inferences/admin.py
+++ backend/inferences/admin.py
@@ -2,5 +2,14 @@ from django.contrib import admin
 
 from inferences.models import ChestXRayInference, COVIDCTInference
 
+
+class COVIDCTInferenceAdmin(admin.ModelAdmin):
+    model = COVIDCTInference
+    list_display = ('id', 'prescriber', 'timestamp', 'diagnosis', 'report',
+                    'is_deleted')
+    list_filter = ('id', 'prescriber', 'timestamp', 'diagnosis', 'report',
+                    'is_deleted')
+
+
 admin.site.register(ChestXRayInference)
-admin.site.register(COVIDCTInference)
+admin.site.register(COVIDCTInference, COVIDCTInferenceAdmin)
