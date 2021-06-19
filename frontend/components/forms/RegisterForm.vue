109e521d1d1fd5db3928037cc697fa25be40cf6b
Parsa <parsa.esfahanian.7596@gmail.com>
2021-06-19 17:54:17 +0430

    added toaster to register page

--- frontend/components/forms/RegisterForm.vue
+++ frontend/components/forms/RegisterForm.vue
@@ -1,61 +1,63 @@
 <template>
-	<div>
-		<form method="post">
-			
+    <div>
+        <form method="post">
             <b-field>
-				<template #label>
-					<span class="has-text-white">Email</span>
-				</template>
-				<b-input
-					type="email"
-					icon="email"
-					placeholder="john@doe.com"
-				>
-				</b-input>
-			</b-field>
-            
-            <b-field>
-				<template #label>
-					<span class="has-text-white">Mobile</span>
-				</template>
-				<b-input
-					icon="phone"
-					placeholder="+989xxxxxxxxx"
-				>
-				</b-input>
-			</b-field>
+                <template #label>
+                    <span class="has-text-white">Email</span>
+                </template>
+                <b-input type="email" icon="email" placeholder="john@doe.com">
+                </b-input>
+            </b-field>
 
             <b-field>
-				<template #label>
-					<span class="has-text-white">National ID</span>
-				</template>
-				<b-input
-					icon="credit-card"
-					placeholder="xxxxxxxxxxx"
-				>
-				</b-input>
-			</b-field>
+                <template #label>
+                    <span class="has-text-white">Mobile</span>
+                </template>
+                <b-input icon="phone" placeholder="+989xxxxxxxxx"> </b-input>
+            </b-field>
 
-			<br />
+            <b-field>
+                <template #label>
+                    <span class="has-text-white">National ID</span>
+                </template>
+                <b-input
+                    icon="credit-card"
+                    placeholder="xxxxxxxxxxx"
+                    password-reveal
+                >
+                </b-input>
+            </b-field>
 
-			<button
-				class="button is-block is-fullwidth is-primary is-medium"
-				type="submit"
-			>
-				Submit
-			</button>
-		</form>
-	</div>
+            <br />
+
+            <button
+                class="button is-block is-fullwidth is-primary is-medium"
+                type="submit"
+                @click="success"
+            >
+                Submit
+            </button>
+        </form>
+    </div>
 </template>
 
 <script>
 export default {
-	name: "RegisterForm",
+    name: "RegisterForm",
+    methods: {
+        success() {
+            this.$buefy.toast.open({
+                message: "Registration submitted!\nPlease check your email.",
+                type: "is-primary",
+                duration: 100000,
+            });
+        },
+    },
 };
 </script>
 
 <style lang="scss" scoped>
 .button {
-	background-color: $primary;
+    background-color: $primary;
 }
 </style>
