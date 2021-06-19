92ca3ccc1c7182d9b4c58f88fbe4aea9d74ac51c
Parsa <parsa.esfahanian.7596@gmail.com>
2021-06-19 17:23:18 +0430

    added registration page

---
+++ frontend/components/forms/RegisterForm.vue
@@ -0,0 +1,61 @@
+<template>
+	<div>
+		<form method="post">
+			
+            <b-field>
+				<template #label>
+					<span class="has-text-white">Email</span>
+				</template>
+				<b-input
+					type="email"
+					icon="email"
+					placeholder="john@doe.com"
+				>
+				</b-input>
+			</b-field>
+            
+            <b-field>
+				<template #label>
+					<span class="has-text-white">Mobile</span>
+				</template>
+				<b-input
+					icon="phone"
+					placeholder="+989xxxxxxxxx"
+				>
+				</b-input>
+			</b-field>
+
+            <b-field>
+				<template #label>
+					<span class="has-text-white">National ID</span>
+				</template>
+				<b-input
+					icon="credit-card"
+					placeholder="xxxxxxxxxxx"
+				>
+				</b-input>
+			</b-field>
+
+			<br />
+
+			<button
+				class="button is-block is-fullwidth is-primary is-medium"
+				type="submit"
+			>
+				Submit
+			</button>
+		</form>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "RegisterForm",
+};
+</script>
+
+<style lang="scss" scoped>
+.button {
+	background-color: $primary;
+}
+</style>
---
+++ frontend/pages/register.vue
@@ -0,0 +1,62 @@
+<template>
+    <section class="hero is-fullheight">
+        <div class="hero-body">
+            <div class="login">
+                <div class="has-text-centered">
+                    <NuxtLink to="/">
+                        <img
+                            src="https://bulma.io/images/bulma-logo-white.png"
+                            width="300px"
+                        />
+                    </NuxtLink>
+                </div>
+
+                <hr />
+
+                <RegisterForm />
+
+                <hr />
+                <nav class="level">
+                    <div class="level-item has-text-centered">
+                        <div>
+                            <h1 class="has-text-light">
+                                Already have an account?
+                                <NuxtLink to="/login">Log in</NuxtLink>
+                            </h1>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </div>
+        <Footer />
+    </section>
+</template>
+
+<script>
+import RegisterForm from "@/components/forms/RegisterForm";
+import Footer from "@/components/layout/Footer";
+
+export default {
+    layout: "bare",
+    components: {
+        RegisterForm,
+        Footer,
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+.hero {
+    background-color: $dogeblueprimary;
+}
+
+.hero-body {
+    justify-content: center;
+}
+
+.login {
+    border-radius: 25px;
+    padding: 1.5rem;
+    box-shadow: 8px 8px 15px #151a21, -8px -8px 15px #1b2430;
+}
+</style>
